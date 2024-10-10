e9f2fc2fef397ed5e63f71dd5c0ae46a58dd7772
Lenovo <Lenovo@PC-JEY>
2024-10-09 21:11:48 -0500

    SEMANA09

--- src/tareasemana09/Ejercicio03.java
+++ src/tareasemana09/Ejercicio03.java
@@ -18,43 +18,45 @@ La compra menor es : 2140.0
  */
 import java.util.Scanner;
 import java.text.DecimalFormat;
+
 public class Ejercicio03 {
 
-    private static int i;
-    public static void main(String[]args){
-        //declarar las variables
+    public static void main(String[] args) {
+        // Declarar las variables
         int n;
-        double comp,totalcomp,promediocomp,compmayor=0.0,compmenor=0.0,acumcomp=0.0;
-         Scanner lectura=new Scanner(System.in);
-         DecimalFormat formato=new DecimalFormat("##.##");
-         System.out.print("Ingrese la cantidad de personas : ");
-         n=lectura.nextInt();
-         double[] arregcomp=new double[n];
-         
-         // entrada de datos
-          for (int i = 0; i < n; i++) {
-              System.out.print("Ingrese las compras "+(i+1)+" : ");
-              comp=lectura.nextDouble();
-              arregcomp[i]=comp;
-              if (comp>compmayor) {
-                  compmayor=comp;
-                  
-              } else {
-                  compmenor=comp;
-              }  
-              }
-                //Proceso de datos
-          for (int i = 0; i < n; i++) {
-              acumcomp+=arregcomp[i];
-            
+        double comp, totalcomp, promediocomp = 0.0, compmayor = 0.0, compmenor = Double.MAX_VALUE, acumcomp = 0.0;
+        Scanner lectura = new Scanner(System.in);
+        DecimalFormat formato = new DecimalFormat("##.##");
+        
+        System.out.print("Ingrese la cantidad de personas : ");
+        n = lectura.nextInt();
+        double[] arregcomp = new double[n];
+        
+        // Entrada de datos
+        for (int i = 0; i < n; i++) {
+            System.out.print("Ingrese las compras " + (i + 1) + " : ");
+            comp = lectura.nextDouble();
+            arregcomp[i] = comp;
+
+            // Calcular compra mayor y menor
+            if (comp > compmayor) {
+                compmayor = comp;
+            }
+            if (comp < compmenor) {
+                compmenor = comp;
+            }
         }
-          totalcomp=acumcomp;
-          promediocomp=totalcomp/n;
-          
-          System.out.println("El total de las compras es  : "+totalcomp);
-          System.out.println("El promedio de compras es : "+formato.format("promediocomp"));
-          System.out.println("La compra mayor es : "+compmayor);
-          System.out.println("La compra menor es : "+compmenor);
+
+        // Proceso de datos
+        for (int i = 0; i < n; i++) {
+            acumcomp += arregcomp[i];
+        }
+        totalcomp = acumcomp;
+        promediocomp = totalcomp / n;
+
+        System.out.println("El total de las compras es  : " + totalcomp);
+        System.out.println("El promedio de compras es : " + formato.format(promediocomp));
+        System.out.println("La compra mayor es : " + compmayor);
+        System.out.println("La compra menor es : " + compmenor);
     }
-    
 }
